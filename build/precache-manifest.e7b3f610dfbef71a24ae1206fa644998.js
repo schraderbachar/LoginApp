5f7097d113611159f8ae48845a59d73652edef8b
schraderbachar <44781569+schraderbachar@users.noreply.github.com>
2020-05-20 16:43:46 -0400

    Add files via upload
    build folder and server fixed and json

---
+++ build/precache-manifest.e7b3f610dfbef71a24ae1206fa644998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18aaee41689d70f77c9ca547aa6709a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d86f68bb20ec1b39302",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "026096887ddc12c52e2c",
+    "url": "/static/js/2.0629c1b6.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.0629c1b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d86f68bb20ec1b39302",
+    "url": "/static/js/main.789f561d.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
--- server.js
+++ server.js
@@ -39,10 +39,10 @@ app.use('/api/users', users)
 //serve static assests if in production
 if (process.env.NODE_ENV === 'production') {
     //set static folder
-    app.use(express.static('client/public'))
+    app.use(express.static('client/build'))
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'public', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
